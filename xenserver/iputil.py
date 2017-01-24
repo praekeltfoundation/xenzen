d89ec8c9530f459e127f2d191fb13ff4b321a61f
Jamie Hewland <jhewland@gmail.com>
2017-01-24 14:37:31 +0200

    xenserver.iputil: Fix flake8 problems

--- xenserver/iputil.py
+++ xenserver/iputil.py
@@ -7,8 +7,10 @@ import struct
 def iptos(ipl):
     return socket.inet_ntoa(struct.pack('!L', ipl))
 
+
 def stoip(s):
     return struct.unpack('!L', socket.inet_aton(s))[0]
+
 
 def ipcalc(subnet):
     b = subnet.strip().split('/')
@@ -19,35 +21,42 @@ def ipcalc(subnet):
     last = ipnl + (2**(32-cidr) - 2)
     return ipnl, first, last, cidr
 
+
 def expandSubnet(subnet):
     ipnl, first, last, cidr = ipcalc(subnet)
     iplist = [iptos(i) for i in range(first, last+1)]
     return iplist
+
 
 def firstRemaining(subnet, used):
     ips = list(set(expandSubnet(subnet)).difference(used))
     ips.sort()
     return ips[0]
 
+
 def allRemaining(subnet, used):
     ips = list(set(expandSubnet(subnet)).difference(used))
     ips.sort()
     return ips
+
 
 def getGateway(subnet):
     ipnl, first, last, cidr = ipcalc(subnet)
 
     return iptos(first)
 
+
 def getNetmask(subnet):
     b = subnet.strip().split('/')
     cidr = int(b[1])
-    return socket.inet_ntoa(struct.pack('!L', 0xffffffff ^ (1 << 32 - cidr) - 1))
+    return socket.inet_ntoa(
+        struct.pack('!L', 0xffffffff ^ (1 << 32 - cidr) - 1))
+
 
 def getSubnet(ip):
     b = ip.strip().split('/')
     ip = b[0]
     cidr = int(b[1])
     ipnl = struct.unpack('!L', socket.inet_aton(ip))[0]
-    network = ipnl & (2**cidr-1)<<(32-cidr)
+    network = ipnl & (2**cidr-1) << (32-cidr)
     return '%s/%s' % (iptos(network), cidr)
