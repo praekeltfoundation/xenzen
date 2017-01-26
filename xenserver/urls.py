b998f349fbce789759f47f25a0f8c0c648f58aa1
Jamie Hewland <jhewland@gmail.com>
2017-01-26 14:49:44 +0200

    urls: admin.autodiscover() not needed on Django 1.7+

--- xenserver/urls.py
+++ xenserver/urls.py
@@ -1,9 +1,6 @@
 from django.conf.urls import patterns, include, url
 from django.views.generic import RedirectView
-
-# Uncomment the next two lines to enable the admin:
 from django.contrib import admin
-admin.autodiscover()
 
 urlpatterns = patterns(
     '',
